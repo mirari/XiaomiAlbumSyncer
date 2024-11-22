6464f6eb7076efef7430a02d0c314f008324d70c
YangYiFan <Coolfan1024@gmail.com>
2024-11-22 21:46:32 +0800

    feat: 批量相册下载

--- src/model/album.py
+++ src/model/album.py
@@ -1,4 +1,5 @@
-from peewee import Model, IntegerField, CharField
+from peewee import Model, IntegerField, CharField, BooleanField
+
 from src.model.db import db
 
 
@@ -6,6 +7,8 @@ class Album(Model):
     id = IntegerField(primary_key=True)
     media_count = IntegerField(default=0)
     name = CharField()
+    # 当 selected 为 True 时，表示用户选择了该相册，将会在非交互式任务中下载
+    selected = BooleanField(default=False)
 
     class Meta:
         database = db
