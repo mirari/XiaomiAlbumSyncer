c70287c55ab6ecc242595226f22f492d627fdc61
Coolfan <coolfan1024@gmail.com>
2025-10-12 18:10:02 +0800

    docs: 加注释

--- server/src/main/kotlin/com/coooolfan/xiaomialbumsyncer/model/AlbumTimeline.kt
+++ server/src/main/kotlin/com/coooolfan/xiaomialbumsyncer/model/AlbumTimeline.kt
@@ -11,6 +11,14 @@ data class AlbumTimeline(
 
 val EMPTY_ALBUM_TIMELINE = AlbumTimeline("-1", emptyMap())
 
+/**
+ * 计算两个相册时间线的差异
+ * 如果两个时间线的indexHash相同，返回空Map（表示没有差异）
+ * 否则，计算每个日期对应的媒体数量差值
+ *
+ * @param other 另一个相册时间线
+ * @return 日期到数量差值的映射，只包含非零差值
+ */
 operator fun AlbumTimeline.minus(other: AlbumTimeline): Map<LocalDate, Long> {
     if (this.indexHash == other.indexHash) return emptyMap()
     val allKeys = this.dayCount.keys + other.dayCount.keys
